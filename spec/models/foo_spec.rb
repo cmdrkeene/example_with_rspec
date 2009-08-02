20ae49b025a1ffc711565169448d218db0b333cd
Brandon Keene <bkeene@gmail.com>
2009-08-02 18:17:00 -0400

    Changing to test

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Foo do
   it "should have a failing example" do
-    false.should be_true
+    true.should_not be_true
   end
 
   it "should have passing example" do
