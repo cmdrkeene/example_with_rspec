c8110a3e91752fb7a333fe332fc01b41fc0c9402
Brandon Keene <bkeene@gmail.com>
2009-08-02 16:14:58 -0400

    Rearrange things

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Foo do
-  it "should have passing example" do
-    true.should be_true
-  end
-  
   it "should have a failing example" do
     false.should be_true
   end
+
+  it "should have passing example" do
+    true.should be_true
+  end  
 end
